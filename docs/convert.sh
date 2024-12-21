40dda162ded05bdf15c0be4ce134f4dbb72985d3
giuseppe <capassog97@gmail.com>
2024-12-21 21:23:49 +0100

    better convert script

--- docs/convert.sh
+++ docs/convert.sh
@@ -1,1 +1,8 @@
-pandoc main.tex -f latex -t mediawiki --metadata link-citations --bibliography=refs.bib --csl=ieee.csl  --citeproc
+pandoc main.tex \
+  -f latex \
+  -t mediawiki \
+  --metadata link-citations \
+  --bibliography=refs.bib \
+  --csl=ieee.csl  \
+  --citeproc | python scripts/adjust.py | tee output.mediawiki | xclip -selection clipboard
+
